097fa31647fa4f0261e44cbd74776f0794eb359a
Bill Song <voidcaster.michaelwang@gmail.com>
2024-04-06 16:00:58 -0700

    add type annotations to bubble

--- bubbleSearch.py
+++ bubbleSearch.py
@@ -1,12 +1,13 @@
+from typing import Any
 # you can use this to sort strings too
-def bubble_sort(elements, key: str) -> None:
-    size = len(elements)
+def bubble_sort(elements: list[Any], key: str) -> None:
+    size:int = len(elements)
 
     for i in range(size-1):
-        swapped = False
+        swapped: bool = False
         for j in range(size-1-i):
             if elements[j][key] > elements[j+1][key]:
-                tmp = elements[j]
+                tmp: Any = elements[j]
                 elements[j] = elements[j+1]
                 elements[j+1] = tmp
                 swapped = True
